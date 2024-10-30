8c788db13fabdb1415ec60b663d0e7cf9de0111c
Skully <contact@skully.tech>
2024-10-30 23:45:14 +0000

    fix: invalid ref to localStorage

--- islands/ThemeButton.tsx
+++ islands/ThemeButton.tsx
@@ -8,11 +8,14 @@ enum Theme {
 
 // Store the theme in local storage.
 const ThemeStorageKey = "rtttl_theme";
+const DefaultTheme = Theme.Dark;
 
 export default function ThemeButton() {
-	const theme = useSignal<Theme>(localStorage.getItem(ThemeStorageKey) as Theme || Theme.Dark);
+	const theme = useSignal<Theme>(DefaultTheme);
 
 	useEffect(() => {
+		// Load the theme from local storage.
+		theme.value = localStorage.getItem(ThemeStorageKey) as Theme || DefaultTheme;
 		document.body.classList.remove(Theme.Dark, Theme.Light);
 		document.body.classList.add(theme.value);
 		localStorage.setItem(ThemeStorageKey, theme.value);
