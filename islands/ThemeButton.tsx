5087b8961d6d3d08c8421e6f5d0c6b743189490f
Skully <contact@skully.tech>
2024-10-31 01:07:46 +0000

    fix: theme switching

--- islands/ThemeButton.tsx
+++ islands/ThemeButton.tsx
@@ -6,26 +6,20 @@ enum Theme {
 	Light = "theme-light",
 }
 
-// Store the theme in local storage.
 const ThemeStorageKey = "rtttl_theme";
 const DefaultTheme = Theme.Dark;
 
 export default function ThemeButton() {
-	const theme = useSignal<Theme>(DefaultTheme);
+	const theme = useSignal<Theme>(localStorage.getItem(ThemeStorageKey) as Theme || DefaultTheme);
 
 	useEffect(() => {
-		// Load the theme from local storage.
-		theme.value = localStorage.getItem(ThemeStorageKey) as Theme || DefaultTheme;
-		document.body.classList.remove(Theme.Dark, Theme.Light);
-		document.body.classList.add(theme.value);
+		document.body.className = theme.value;
 		localStorage.setItem(ThemeStorageKey, theme.value);
 	}, [theme.value]);
 
 	return (
 		<a
-			onClick={() => {
-				theme.value = (theme.value === Theme.Dark) ? Theme.Light : Theme.Dark;
-			}}
+			onClick={() => theme.value = theme.value === Theme.Dark ? Theme.Light : Theme.Dark}
 			class="nav-link px-0"
 			data-bs-toggle="tooltip"
 			data-bs-placement="bottom"
